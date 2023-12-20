d5d81dd2a1371708654eafe610ca143873ff818a
bindu <sindhudesalanka@gmail.com>
2023-12-20 14:18:45 +0530

    a

--- main.tf
+++ main.tf
@@ -4,12 +4,13 @@ resource "aws_ssm_parameter" "foo" {
   type  = var.params[count.index].type
   value = var.params[count.index].value
 }
+
 variable "params" {
   default = [
-    { name:"roboshop.dev.frontend.catalogue_url", type:"String",value:"http://catalogue-dev.tejesh.cloud:8080/"},
+    { name:"roboshop.dev.frontend.catalogue_url", type:"String", value:"http://catalogue-dev.tejesh.cloud:8080/"},
     { name:"roboshop.dev.frontend.user_url", type:"String",value:"http://user-dev.tejesh.cloud:8080/"},
     { name:"roboshop.dev.frontend.cart_url", type:"String",value:"http://cart-dev.tejesh.cloud:8080/"},
     { name:"roboshop.dev.frontend.shipping_url", type:"String",value:"http://shipping-dev.tejesh.cloud:8080/"},
-    { name:"roboshop.dev.frontend.payment_url", type:"String",value:"http://payment-dev.tejesh.cloud:8080/"},
+    { name:"roboshop.dev.frontend.payment_url", type:"String",value:"http://payment-dev.tejesh.cloud:8080/"}
   ]
 }
