5d578fbb65a46b9f631b1b36613561a8cfd5a94f
bindu <sindhudesalanka@gmail.com>
2023-12-20 12:07:58 +0530

    a

--- main.tf
+++ main.tf
@@ -6,10 +6,10 @@ resource "aws_ssm_parameter" "foo" {
 }
 variable "params" {
   default = [
-    { name:"roboshop.dev.frontend.catalogue_url", type:"String",value:"http://catalogue-dev.tejesh.cloud:8080/"},
-    { name:"roboshop.dev.frontend.user_url", type:"String",value:"http://user-dev.tejesh.cloud:8080/"},
-    { name:"roboshop.dev.frontend.cart_url", type:"String",value:"http://cart-dev.tejesh.cloud:8080/"},
-    { name:"roboshop.dev.frontend.shipping_url", type:"String",value:"http://shipping-dev.tejesh.cloud:8080/"},
-    { name:"roboshop.dev.frontend.payment_url", type:"String",value:"http://payment-dev.tejesh.cloud:8080/"},
+    { name:"roboshop.dev.frontend.catalogue_url", type:"String",value:"http://catalogue.dev.tejesh.cloud:8080/"},
+    { name:"roboshop.dev.frontend.user_url", type:"String",value:"http://user.dev.tejesh.cloud:8080/"},
+    { name:"roboshop.dev.frontend.cart_url", type:"String",value:"http://cart.dev.tejesh.cloud:8080/"},
+    { name:"roboshop.dev.frontend.shipping_url", type:"String",value:"http://shipping.dev.tejesh.cloud:8080/"},
+    { name:"roboshop.dev.frontend.payment_url", type:"String",value:"http://payment.dev.tejesh.cloud:8080/"},
   ]
 }
