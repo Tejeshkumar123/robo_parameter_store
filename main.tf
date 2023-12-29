19c0648e3805beab9369534ac99ff37f3ceaf693
bindu <sindhudesalanka@gmail.com>
2023-12-29 10:02:03 +0530

    a

--- main.tf
+++ main.tf
@@ -1,4 +1,5 @@
-resource "aws_ssm_parameter" "foo" {
+############## create aws parameter in paremeter store terraform ##################
+resource "aws_ssm_parameter" "params" {
   count = length(var.params)
   name  = var.params[count.index].name
   type  = var.params[count.index].type
@@ -7,10 +8,10 @@ resource "aws_ssm_parameter" "foo" {
 
 variable "params" {
   default = [
-    { name:"roboshop.dev.frontend.catalogue_url", type:"String", value:"http://catalogue-dev.tejesh.cloud:8080/"},
-    { name:"roboshop.dev.frontend.user_url", type:"String",value:"http://user-dev.tejesh.cloud:8080/"},
-    { name:"roboshop.dev.frontend.cart_url", type:"String",value:"http://cart-dev.tejesh.cloud:8080/"},
-    { name:"roboshop.dev.frontend.shipping_url", type:"String",value:"http://shipping-dev.tejesh.cloud:8080/"},
-    { name:"roboshop.dev.frontend.payment_url", type:"String",value:"http://payment-dev.tejesh.cloud:8080/"}
+    { name = "roboshop.dev.frontend.catalogue_url", type = "String", value = "http://catalogue-dev.tejesh.cloud:8080/" },
+    { name = "roboshop.dev.frontend.user_url", type = "String", value = "http://user-dev.tejesh.cloud:8080/" },
+    { name = "roboshop.dev.frontend.cart_url", type = "String", value = "http://cart-dev.tejesh.cloud:8080/" },
+    { name = "roboshop.dev.frontend.payment_url", type = "String", value = "http://payment-dev.tejesh.cloud:8080/" },
+    { name = "roboshop.dev.frontend.shipping_url", type = "String", value = "http://shipping-dev.tejesh.cloud:8080/" }
   ]
 }
